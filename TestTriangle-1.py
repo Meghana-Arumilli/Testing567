94381d2386ae72e413a5acd594b2e35538f4ad90
Arumilli Meghana <meghanamaggi2020@gmail.com>
2023-09-27 23:19:49 -0400

    first commit

--- TestTriangle-1.py
+++ TestTriangle-1.py
@@ -26,6 +26,26 @@ class TestTriangles(unittest.TestCase):
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
 
+    def testRightTriangleA(self): 
+        self.assertEqual(classifyTriangle(6,1,9),'Right','3,4,5 is a Right triangle')
+
+    def testRightTriangleB(self): 
+        self.assertEqual(classifyTriangle(9,8,7),'Right','5,3,4 is a Right triangle')
+        
+    def testEquilateralTriangles(self): 
+        self.assertEqual(classifyTriangle(5,5,5),'Equilateral','1,1,1 should be equilateral')
+
+    def test_triangle(self):
+        self.assertEqual(classify_triangle(3, 3, 4), "Isosceles")
+        self.assertEqual(classify_triangle(8, 14, 17), "Scalene")
+        self.assertEqual(classify_triangle(30, 30, 30), "Equilateral")
+
+
+if __name__ == "__main__":
+    classify_triangle(2,5,9)
+    classify_triangle(3,3,3)
+    unittest.main()
+
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
